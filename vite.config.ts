bffe66e5a0acafd359eeb4a7c5cf72b0c2aa0a19
myunggyuns <sonmg90@gmail.com>
2026-08-08 22:51:39 +0900

    vite config 수정

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/phaser')) {
+            return 'phaser'
+          }
+        },
+      },
+    },
+  },
 })
