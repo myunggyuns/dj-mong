efead9266db293fc7697a3d326e01cbf6c0041f6
myunggyuns <sonmg90@gmail.com>
2026-08-08 23:17:42 +0900

    CSS file read 에러

--- src/screens/tutorial/TutorialScreen.tsx
+++ src/screens/tutorial/TutorialScreen.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useGameStore } from '../../store/gameStore';
 import { driver } from 'driver.js';
 import 'driver.js/dist/driver.css';
-import './TutorialScreen.module.css';
+import './TutorialScreen.css';
 import { STEPS } from '../../constant/tutorial';
 
 export default function TutorialScreen() {
